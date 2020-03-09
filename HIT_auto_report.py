48d60b072837cc4a9846f269b6c5ee58272b2657
xrervip <48290088+xrervip@users.noreply.github.com>
2020-03-09 12:49:33 +0800

    新增命令行读入账号密码

--- HIT_auto_report.py
+++ HIT_auto_report.py
@@ -7,9 +7,11 @@
 #警告：在使用本工具之前请确保您没有感染新冠肺炎或者存在感染的风险或处于医学隔离观察中，代码会根据您之前保留的填写记录自动提交
 #本工具开发的目的是为了配合导员工作（作者经常因为没有及时上报被辅导员提醒），对于不遵守使用条例的行为而导致的后果作者不承担责任
 #最后祝伟大祖国早日战胜疫情，武汉加油，中国加油！
+#2020/03/09 新增命令行读入账号密码 格式： 学号 密码
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+import sys
 
 class Report(object):
     def __init__(self):
@@ -57,6 +59,11 @@ class Report(object):
 if __name__ == '__main__':
     id=""
     Password=""
+    if len(sys.argv)==3:
+        id=sys.argv[1]
+        Password=sys.argv[2]
+        r = Report()
+        sys.exit(0)
     try:
         file=open('./账户密码.txt', 'r')
         id = file.readline()
